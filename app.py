cb6dc32781863153d6f591963e6d066c2336277c
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-03-02 22:09:46 -0800

    added goodbye route

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/hello')
+def say_hello():
+    """Return simple "Hello" Greeting."""
+
+    html = "<html><body><h1>Hello World! I am coming alive!</h1></body></html>"
+    return html
+
+@app.route('/goodbye')
+def say_goodbye():
+    """Return simple "Goodbye" Greeting."""
+
+    html = "<html><body><h1>Goodbye World! I am leaving for another route for now!</h1></body></html>"
+    return html
